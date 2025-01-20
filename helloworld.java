cfe3d05c8389ece689971da6387941f7c6718db0
SanketJuikar1994 <sanketjuikar26@gmail.com>
2025-01-20 15:05:37 +0530

    check auto trigger

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 class Simple {
     public static void main(String[] args) {
-        System.out.println("Hello, World Devops Jenkins practice!"); 
+        System.out.println("Hello, World Devops Jenkins practice!....."); 
     }
 }
